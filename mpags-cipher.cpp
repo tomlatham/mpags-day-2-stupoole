d941ed7ec8c4e7521182ed634672eacfc6b0c4d1
Stu Poole <stuart.f.poole@gmail.com>
2019-10-27 16:07:33 +0100

    Modularised the two previously defines functions. This required adding header files and source files, including dependencies in these. These were then added to CMAKELISTS and to the main source file.

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -2,104 +2,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include "processCommandLine.h"
+#include "transformChar.h"
 // For std::isalpha and std::isupper
 #include <cctype>
 
-std::string transformChar (const char in_char) {
-  // Uppercase alphabetic characters
-  if (std::isalpha(in_char))
-  {
-    std::string str;
-    return str += std::toupper(in_char);
-  }
-
-  // Transliterate digitSs to English words
-  switch (in_char)
-  {
-  case '0':
-    return "ZERO";
-  case '1':
-    return "ONE";
-  case '2':
-    return "TWO";
-  case '3':
-    return "THREE";
-  case '4':
-    return "FOUR";
-  case '5':
-    return "FIVE";
-  case '6':
-    return "SIX";
-  case '7':
-    return "SEVEN";
-  case '8':
-    return "EIGHT";
-  case '9':
-    return "NINE";
-  default:
-    return "";
-  }
-
-  // If the character isn't alphabetic or numeric, DONT add it.
-  // Our ciphers can only operate on alphabetic characters.
-
-}
-
-bool processCommandLine(const std::vector<std::string>& args,
-  bool& helpRequested,
-  bool& versionRequested,
-  std::string& inputFileName,
-  std::string& outputFileName)
-{
-  typedef std::vector<std::string>::size_type size_type;
-  const size_type nCmdLineArgs {args.size()};
-
-  for (size_type i {1}; i < nCmdLineArgs; ++i)
-  {
-
-    if (args[i] == "-h" || args[i] == "--help") {
-      helpRequested = true;
-    }
-    else if (args[i] == "--version") {
-      versionRequested = true;
-    }
-    else if (args[i] == "-i") {
-      // Handle input file option
-      // Next element is filename unless -i is the last argument
-      if (i == nCmdLineArgs-1) {
-        std::cerr << "[error] -i requires a filename argument" << std::endl;
-        // exit main with non-zero return to indicate failure
-        return true;
-      }
-      else {
-        // Got filename, so assign value and advance past it
-        inputFileName = args[i+1];
-        ++i;
-      }
-    }
-    else if (args[i] == "-o") {
-      // Handle output file option
-      // Next element is filename unless -o is the last argument
-      if (i == nCmdLineArgs-1) {
-        std::cerr << "[error] -o requires a filename argument" << std::endl;
-        // exit main with non-zero return to indicate failure
-        return true;
-      }
-      else {
-        // Got filename, so assign value and advance past it
-        outputFileName = args[i+1];
-        ++i;
-      }
-    }
-    else {
-      // Have an unknown flag to output error message and return non-zero
-      // exit status to indicate failure
-      std::cerr << "[error] unknown argument '" << args[i] << "'\n";
-      return false;
-    }
-  }
-}
 
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
