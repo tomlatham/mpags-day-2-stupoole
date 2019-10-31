39f7417bd1084380bfa2f7c4b03c1d104ceafdd8
Thomas Latham <T.Latham@warwick.ac.uk>
2019-10-31 23:02:22 +0000

    TL review of day 2 homework

--- MPAGSCipher/runCaeserCipher.cpp
+++ MPAGSCipher/runCaeserCipher.cpp
@@ -7,17 +7,20 @@ std::string runCaeserCipher(const std::string& input_text, const size_t key, con
 {
   const std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const std::string::size_type alphabetSize {alphabet.size()};
+
+  const size_t truncatedKey { key % alphabetSize };
+
   std::string output_string;
   if (decrypt)
   {
     for (const char in_char : input_text)
     {
-      output_string += alphabet[(alphabetSize + alphabet.find(in_char) - key)%alphabetSize];
+      output_string += alphabet[(alphabetSize + alphabet.find(in_char) - truncatedKey)%alphabetSize];
     }
   } else {
     for (const char in_char : input_text)
     {
-      output_string += alphabet[(alphabet.find(in_char) + key)%alphabetSize];
+      output_string += alphabet[(alphabet.find(in_char) + truncatedKey)%alphabetSize];
     }
   }
   return output_string;
